0a1d834a2dd2036077ccb3edccb4584aee5ffd8d
Jason Jewell <jasonjewell@truevisionstrategies.com>
2020-10-28 20:45:06 -0700

    Added seeders and factories for comments and user_tags

---
+++ database/seeders/UserTagsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\UserTags;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class UserTagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('user_tags')->insert([
+          'post_id' => 1,
+          'user_id' => 1,
+          'created_at' => now(),
+          'updated_at' => now()
+      ]);
+
+      factory(UserTags::class, 300)->create();
+    }
+}
