6cc24b89ba3a3d95c3120347b4920c5369956600
Kyle Dean <kdean003@ucr.edu>
2020-10-28 22:05:29 -0700

    fix factories

--- database/factories/FollowersFactory.php
+++ database/factories/FollowersFactory.php
@@ -1,16 +1,15 @@
 <?php
 
-
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\Followers;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
-
 $factory->define(Followers::class, function (Faker $faker) {
     return [
-        'user_id' => $faker->numberBetween(1,300),
-        'follower_user_id' => $this->$faker->numberBetween(1,300),
-        'created_at' => $this->$faker->date,
-        'updated_at' => $this->$faker->date,
+        'user_id' => $this->faker->numberBetween(1,300),
+        'follower_user_id' => $this->faker->numberBetween(1,300),
+        'created_at' => $this->faker->date,
+        'updated_at' => $this->faker->date,
     ];
 });
--- database/factories/FollowingFactory.php
+++ database/factories/FollowingFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\Following;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
@@ -8,9 +8,9 @@ use Faker\Generator as Faker;
 
 $factory->define(Following::class, function (Faker $faker) {
     return [
-        'user_id' => $faker->numberBetween(1,300),
-        'follower_user_id' => $this->$faker->numberBetween(1,300),
-        'created_at' => $this->$faker->date,
-        'updated_at' => $this->$faker->date,
+        'user_id' => $this->faker->numberBetween(1,300),
+        'follower_user_id' => $this->faker->numberBetween(1,300),
+        'created_at' => $this->faker->date,
+        'updated_at' => $this->faker->date,
     ];
 });
