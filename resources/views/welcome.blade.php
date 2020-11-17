565abd79a3958ab4b87b2c7abec459c52f83f969
Kyle Dean <kdean003@ucr.edu>
2020-11-16 18:44:32 -0800

    Changed welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
 <div class="container" style="height: auto;">
   <div class="row justify-content-center">
       <div class="col-lg-7 col-md-8">
-          <h1 class="text-white text-center">{{ __('Jason is a PHP guru.') }}</h1>
+          <h1 class="text-white text-center">{{ __('Instagram 179') }}</h1><br>
+          <h2 class="text-white text-center">Made By</h2><br>
+          <h2 class="text-white text-center">Jason Jewell<br>Christian Campos<br>Robert Arenas<br>Kyle Dean</h3>
       </div>
   </div>
 </div>
