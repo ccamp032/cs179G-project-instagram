a4d0aeab591bf0e1399700d335791b15003d16e7
Jason Jewell <jasonjewell@truevisionstrategies.com>
2020-11-25 18:35:20 -0800

    added banner container

--- resources/views/profile/myprofile.blade.php
+++ resources/views/profile/myprofile.blade.php
@@ -3,6 +3,22 @@
 @section('content')
   <div class="content">
     <div class="container-fluid">
+      <div class="row" style="padding-left:15px; padding-right:15px;">
+        <div class="card card-stats">
+          <div class="card-header card-header-success card-header-icon">
+            <div class="card-icon">
+              <i class="material-icons">supervisor_account</i>
+            </div>
+            <p class="card-category">Followers</p>
+            <h3 class="card-title">{{count($myFollowers)}}</h3>
+          </div>
+          <div class="card-footer">
+            <div class="stats">
+              <i class="material-icons"></i>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="row">
         <div class="col-lg-3 col-md-6 col-sm-6">
           <div class="card card-stats">
