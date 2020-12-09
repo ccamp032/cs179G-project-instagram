06988ec6ee4e8e008df1e849b8378a43dfd9afc2
Kyle Dean <kdean003@ucr.edu>
2020-12-08 21:56:11 -0800

    fixed another issue with like/dislike

--- resources/views/profile/userprofile.blade.php
+++ resources/views/profile/userprofile.blade.php
@@ -201,21 +201,28 @@
                       <div class="stats-right stats-section">
                         <p style="display:inline" id="post{{ $post['post']['id'] }}LikeCount">{{ $post['likes'] }}</p>
                         @if($post['likedImage'] == 1)
-                        <a style="color:#2196f3;" href="#" id="post{{ $post['post']['id'] }}Like">
-                          <i style="vertical-align: text-bottom;" class="material-icons">thumb_up</i>
-                        </a>
+                          <a style="color:#2196f3;" href="#" id="post{{ $post['post']['id'] }}Like">
+                            <i style="vertical-align: text-bottom;" class="material-icons">thumb_up</i>
+                          </a>
                           <a style="color:#6c757d;" href="#" id="post{{ $post['post']['id'] }}Dislike">
                             <i style="vertical-align: text-bottom;" class="material-icons">thumb_down</i>
                           </a>
-                          @else
+                        @elseif($post['likedImage'] == 0)
                           <a style="color:#6c757d;" href="#" id="post{{ $post['post']['id'] }}Like">
                             <i style="vertical-align: text-bottom;" class="material-icons">thumb_up</i>
                           </a>
-                            <a style="color:#f44336;" href="#" id="post{{ $post['post']['id'] }}Dislike">
-                              <i style="vertical-align: text-bottom;" class="material-icons">thumb_down</i>
-                            </a>
-                          @endif
-                            <p style="display:inline" id="post{{ $post['post']['id'] }}DislikeCount">{{ $post['dislikes'] }}</a>
+                          <a style="color:#f44336;" href="#" id="post{{ $post['post']['id'] }}Dislike">
+                            <i style="vertical-align: text-bottom;" class="material-icons">thumb_down</i>
+                          </a>
+                        @else
+                          <a style="color:#6c757d;" href="#" id="post{{ $post['post']['id'] }}Like">
+                            <i style="vertical-align: text-bottom;" class="material-icons">thumb_up</i>
+                          </a>
+                          <a style="color:#6c757d;" href="#" id="post{{ $post['post']['id'] }}Dislike">
+                            <i style="vertical-align: text-bottom;" class="material-icons">thumb_down</i>
+                          </a>
+                        @endif
+                        <p style="display:inline" id="post{{ $post['post']['id'] }}DislikeCount">{{ $post['dislikes'] }}</a>
                       </div>
                     </div>
                   </div>
