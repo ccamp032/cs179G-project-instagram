21b57d9ff55b1c80ad7ff5d770195ef8b0ba551b
Kyle Dean <kdean003@ucr.edu>
2020-12-13 14:53:57 -0800

    implemented calendar for date search filter

--- resources/views/layouts/navbars/navs/auth.blade.php
+++ resources/views/layouts/navbars/navs/auth.blade.php
@@ -37,66 +37,84 @@
               </select>
             </div>
             <div>
-              <select id="search_method_3" name="search_method_3" style="visibility: hidden; margin-left: 30px; background: #00ffff00; color: grey; border-top: none; border-left: 1px solid grey;
+              <select id="search_method_3" name="search_method_3" style="display: none; margin-left: 30px; background: #00ffff00; color: grey; border-top: none; border-left: 1px solid grey;
                 width: 50%; border-left:none; border-right:none;">
                   <option value="less_than"><</option>
                   <option value="equal_to">=</option>
                   <option value="greater-than">></option>
               </select>
             </div>
-            <script>
-              $('#search_method_1').change(function() {
-                if($(this).val() == "users") {
-                  options = '<option value="name">Name</option> \
-                             <option value="post_description">Post Description</option> \
-                             <option value="follower_count">Follower Count</option> \
-                             <option value="user_tags">User Tags</option> \
-                             <option value="misc_tags">Misc Tags</option>';
-                             $('#search_method_2').css('margin-right', '0px');
-                             $('#search_method_3').css('visibility', 'hidden');
-                }
-                else if($(this).val() == "posts") {
-                  options = '<option value="description">Description</option> \
-                             <option value="user_name">User Name</option> \
-                             <option value="user_tags">User Tags</option> \
-                             <option value="misc_tags">Misc Tags</option> \
-                             <option value="post_views">Views</option> \
-                             <option value="post_date">Date</option>';
-                  $('#search_method_2').css('margin-right', '37px');
-                  $('#search_method_3').css('visibility', 'hidden');
-                }
-                $('#search_method_2').html(options);
-              });
-
-              $('#search_method_2').change(function() {
-                if($(this).val() == "follower_count") {
-                  $('#search_method_3').css('visibility', 'visible');
-                }
-                else if($(this).val() == "like_count") {
-                  $('#search_method_3').css('visibility', 'visible');
-                }
-                else if($(this).val() == "post_count") {
-                  $('#search_method_3').css('visibility', 'visible');
-                }
-                else if($(this).val() == "post_views") {
-                  $('#search_method_3').css('visibility', 'visible');
-                }
-                else if($(this).val() == "comments_count") {
-                  $('#search_method_3').css('visibility', 'visible');
-                }
-                else {
-                  $('#search_method_3').css('visibility', 'hidden');
-                }
-              });
-            </script>
-        </div>
-        <input type="text" value="" class="form-control" name ="search" placeholder="Search...">
+          </div>
+        <input type="date" id="post_date" name="post_date" style="display: none; margin-right: 30px"> 
+        <input type="text" id="search" value="" class="form-control" name ="search" placeholder="Search...">
         <button type="submit" class="btn btn-white btn-round btn-just-icon">
           <i class="material-icons">search</i>
           <div class="ripple-container"></div>
         </button>
         </div>
       </form>
+      <script>
+        $('#search_method_1').change(function() {
+          if($(this).val() == "users") {
+            options = '<option value="name">Name</option> \
+                       <option value="post_description">Post Description</option> \
+                       <option value="follower_count">Follower Count</option> \
+                       <option value="user_tags">User Tags</option> \
+                       <option value="misc_tags">Misc Tags</option>';
+                       $('#search_method_2').css('margin-right', '0px');
+                       $('#search_method_3').css('display', 'none');
+                       $('#post_date').css('display', 'none');
+                       $('#search').css('display', '');
+          }
+          else if($(this).val() == "posts") {
+            options = '<option value="description">Description</option> \
+                       <option value="user_name">User Name</option> \
+                       <option value="user_tags">User Tags</option> \
+                       <option value="misc_tags">Misc Tags</option> \
+                       <option value="post_views">Views</option> \
+                       <option value="post_date">Date</option>';
+            $('#search_method_2').css('margin-right', '37px');
+            $('#search_method_3').css('display', 'none');
+            $('#post_date').css('display', 'none');
+            $('#search').css('display', '');
+          }
+          $('#search_method_2').html(options);
+        });
+
+        $('#search_method_2').change(function() {
+          if($(this).val() == "follower_count") {
+            $('#search_method_3').css('display', '');
+            $('#search').css('display', '');
+          }
+          else if($(this).val() == "like_count") {
+            $('#search_method_3').css('display', '');
+            $('#search').css('display', '');
+          }
+          else if($(this).val() == "post_count") {
+            $('#search_method_3').css('display', '');
+            $('#search').css('display', '');
+          }
+          else if($(this).val() == "post_views") {
+            $('#search_method_3').css('display', '');
+            $('#search').css('display', '');
+          }
+          else if($(this).val() == "comments_count") {
+            $('#search_method_3').css('display', '');
+            $('#search').css('display', '');
+          }
+          else if($(this).val() == "post_date") {
+            $('#search_method_3').css('display', 'none');
+            $('#search').css('display', 'none');
+            $('#post_date').css('display', '');
+          }
+          else {
+            $('#search_method_3').css('display', 'none');
+            $('#post_date').css('display', 'none');
+            $('#search').css('display', '');
+
+          }
+        });
+      </script>
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link" href="{{ route('home') }}">
