20d13c408e8ec6a321b24b9231d8232cb55276c4
Jason Jewell <jasonjewell@truevisionstrategies.com>
2020-12-13 19:44:58 -0800

    removed unneeded category

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -11,7 +11,6 @@
             <div class="card ">
               <div class="card-header card-header-primary">
                 <h4 class="card-title">{{ __('Edit Post') }}</h4>
-                <p class="card-category">{{ __('User information') }}</p>
               </div>
               <div class="card-body ">
                 @if (session('status'))
